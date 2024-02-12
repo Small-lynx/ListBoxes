7e128c09ed3cb92ef62125833f2b3686252efb0f
Small-lynx <lynxrufus2k4@gmail.com>
2024-02-12 17:40:04 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ListBoxes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listLeft = new System.Windows.Forms.ListBox();
+            this.listRight = new System.Windows.Forms.ListBox();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonAllRight = new System.Windows.Forms.Button();
+            this.buttonAllLeft = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listLeft
+            // 
+            this.listLeft.AllowDrop = true;
+            this.listLeft.FormattingEnabled = true;
+            this.listLeft.ItemHeight = 16;
+            this.listLeft.Location = new System.Drawing.Point(12, 12);
+            this.listLeft.Name = "listLeft";
+            this.listLeft.Size = new System.Drawing.Size(180, 196);
+            this.listLeft.TabIndex = 0;
+            this.listLeft.DragDrop += new System.Windows.Forms.DragEventHandler(this.listLeft_DragDrop);
+            this.listLeft.DragEnter += new System.Windows.Forms.DragEventHandler(this.list_DragEnter);
+            this.listLeft.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listLeft_MouseDown);
+            // 
+            // listRight
+            // 
+            this.listRight.AllowDrop = true;
+            this.listRight.FormattingEnabled = true;
+            this.listRight.ItemHeight = 16;
+            this.listRight.Location = new System.Drawing.Point(264, 12);
+            this.listRight.Name = "listRight";
+            this.listRight.Size = new System.Drawing.Size(180, 196);
+            this.listRight.TabIndex = 1;
+            this.listRight.DragDrop += new System.Windows.Forms.DragEventHandler(this.listRight_DragDrop);
+            this.listRight.DragEnter += new System.Windows.Forms.DragEventHandler(this.list_DragEnter);
+            this.listRight.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listRight_MouseDown);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Enabled = false;
+            this.buttonRight.Location = new System.Drawing.Point(207, 52);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(40, 25);
+            this.buttonRight.TabIndex = 2;
+            this.buttonRight.Text = ">";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonAllRight
+            // 
+            this.buttonAllRight.Location = new System.Drawing.Point(207, 81);
+            this.buttonAllRight.Name = "buttonAllRight";
+            this.buttonAllRight.Size = new System.Drawing.Size(40, 25);
+            this.buttonAllRight.TabIndex = 2;
+            this.buttonAllRight.Text = ">>";
+            this.buttonAllRight.UseVisualStyleBackColor = true;
+            this.buttonAllRight.Click += new System.EventHandler(this.buttonAllRight_Click);
+            // 
+            // buttonAllLeft
+            // 
+            this.buttonAllLeft.Location = new System.Drawing.Point(207, 110);
+            this.buttonAllLeft.Name = "buttonAllLeft";
+            this.buttonAllLeft.Size = new System.Drawing.Size(40, 25);
+            this.buttonAllLeft.TabIndex = 2;
+            this.buttonAllLeft.Text = "<<";
+            this.buttonAllLeft.UseVisualStyleBackColor = true;
+            this.buttonAllLeft.Click += new System.EventHandler(this.buttonAllLeft_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Enabled = false;
+            this.buttonLeft.Location = new System.Drawing.Point(207, 139);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(40, 25);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.Text = "<";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 222);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonAllLeft);
+            this.Controls.Add(this.buttonAllRight);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.listRight);
+            this.Controls.Add(this.listLeft);
+            this.Name = "Form1";
+            this.Text = "ListBoxes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listLeft;
+        private System.Windows.Forms.ListBox listRight;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonAllRight;
+        private System.Windows.Forms.Button buttonAllLeft;
+        private System.Windows.Forms.Button buttonLeft;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int listIndex;
+
+        /// <summary>
+        /// Действия при загрузке формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var massLeft = new string[] { "Monday", "Tuesday", "Webnesday" };
+            var massRight = new string[] { "Thurday", "Friday", "Saturday", "Sunday" };
+            listLeft.Items.AddRange(massLeft);
+            listRight.Items.AddRange(massRight);
+        }
+
+        /// <summary>
+        /// Перенос строки из левого листа в правый
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            listRight.Items.Add(listLeft.Items[listLeft.SelectedIndex]);
+            listLeft.Items.RemoveAt(listLeft.SelectedIndex);
+            buttonRight.Enabled = false;
+        }
+
+        /// <summary>
+        /// Перенос всех строк из левого листа в правый
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonAllRight_Click(object sender, EventArgs e)
+        {
+            listRight.Items.AddRange(listLeft.Items);
+            listLeft.Items.Clear();
+            buttonLeft.Enabled = false;
+            buttonRight.Enabled = false;
+        }
+
+        /// <summary>
+        /// Перенос строки из правого листа в левый
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonAllLeft_Click(object sender, EventArgs e)
+        {
+            listLeft.Items.AddRange(listRight.Items);
+            listRight.Items.Clear();
+            buttonLeft.Enabled = false;
+            buttonRight.Enabled = false;
+        }
+
+        /// <summary>
+        /// Перенос всех строк из правого листа в левый
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            listLeft.Items.Add(listRight.Items[listRight.SelectedIndex]);
+            listRight.Items.RemoveAt(listRight.SelectedIndex);
+            buttonLeft.Enabled = false;
+        }
+
+        /// <summary>
+        /// Нажатие кнопки мыши у левого листа
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listLeft_MouseDown(object sender, MouseEventArgs e)
+        {
+            listIndex = 1;
+            if (listLeft.SelectedItem != null)
+            {
+                listLeft.DoDragDrop(listLeft.SelectedItem.ToString(), DragDropEffects.Copy);
+            }
+            if (listLeft.SelectedIndex == -1)
+            {
+                buttonRight.Enabled = false;
+            }
+            else
+            {
+                buttonRight.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Нажатие кнопки мыши у правого листа
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listRight_MouseDown(object sender, MouseEventArgs e)
+        {
+            listIndex = 2;
+            if (listRight.SelectedItem != null)
+            {
+                listLeft.DoDragDrop(listRight.SelectedItem.ToString(), DragDropEffects.Copy);
+            }
+            if (listRight.SelectedIndex == -1)
+            {
+                buttonLeft.Enabled = false;
+            }
+            else
+            {
+                buttonLeft.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Эффекты при перетаскивании
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void list_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// Завершение перетаскивания в правый лист
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listRight_DragDrop(object sender, DragEventArgs e)
+        {
+            if (listIndex == 1)
+            {
+                listRight.Items.Add(e.Data.GetData(DataFormats.Text));
+                listLeft.Items.Remove(e.Data.GetData(DataFormats.Text));
+                listIndex = 0;
+            }
+        }
+
+        /// <summary>
+        /// Завершение перетаскивания в девый лист
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listLeft_DragDrop(object sender, DragEventArgs e)
+        {
+            if (listIndex == 2)
+            {
+                listLeft.Items.Add(e.Data.GetData(DataFormats.Text));
+                listRight.Items.Remove(e.Data.GetData(DataFormats.Text));
+                listIndex = 0;
+            }
+        }
+    }
+}
